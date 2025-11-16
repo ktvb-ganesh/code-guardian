7a214336d209065f3ee5a3c74d18333a40bac30d
ktvb-ganesh <ganeshrocks140@gmail.com>
2025-11-16 17:59:11 +0530

    console necessary details

--- service/route.js
+++ service/route.js
@@ -7,7 +7,16 @@ router.get("/", (req, res) => {
 
 router.post("/api/webhook", (req, res) => {
   console.log("Body keys:", Object.keys(req.body));
-  console.log('body-------------->', res);
+  if (req.headers["x-github-event"] === "push") {
+    const payload = req.body;
+    const repo = payload.repository.full_name;
+    const commit = payload.after;
+    const branch = payload.ref;
+
+    console.log("Repo:", repo);
+    console.log("Commit:", commit);
+    console.log("Branch:", branch);
+  }
 
   res.status(200).send("OK");
 });

6353a1b32a21411743b40683fae422173192afe8
ktvb-ganesh <ganeshrocks140@gmail.com>
2025-11-16 18:00:52 +0530

    check for headers

--- service/route.js
+++ service/route.js
@@ -6,7 +6,7 @@ router.get("/", (req, res) => {
 });
 
 router.post("/api/webhook", (req, res) => {
-  console.log("Body keys:", Object.keys(req.body));
+  console.log('req.headers----', req.headers);
   if (req.headers["x-github-event"] === "push") {
     const payload = req.body;
     const repo = payload.repository.full_name;
